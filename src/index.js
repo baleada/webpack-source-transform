62dd1f4b62bebb74e3235654c9edebd5e5ec6268
AlexVipond <alex.r.vipond@gmail.com>
2019-09-28 18:55:17 -0600

    bug fixes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import Markdownit from 'markdown-it'
+import MarkdownIt from 'markdown-it'
 import fm from 'front-matter'
 import { getOptions } from 'loader-utils'
 import { resolveComponents, renderComponent, toMap } from './util'
 import { defaultOptions } from './stubs'
 
 export default function(source) {
+  this.cacheable()
+
   const body = fm(source).body,
-        options = { defaultOptions, ...getOptions(this) },
-        md = new Markdownit(options.markdownit),
-        components = toMap(resolveComponents(options.components, md.render)),
-        postRender = options.postRender
+        options = { ...defaultOptions, ...getOptions(this) },
+        md = new MarkdownIt(options.markdownit),
+        components = toMap(resolveComponents(options.components, md.render.bind(md))),
+        { postRender } = options
 
   return postRender(
     md.render(
