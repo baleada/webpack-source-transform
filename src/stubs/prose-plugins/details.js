f05e67724387a296181b89e39f62474990ae94ee
AlexVipond <alex.r.vipond@gmail.com>
2019-11-05 19:17:38 -0600

    Details/Summary plugin

--- src/stubs/prose-plugins/details.js
+++ src/stubs/prose-plugins/details.js
@@ -1,1 +1,21 @@
-// TODO: write plugin to support markdown details/summary
+// details (^^^ ...props)
+import MarkdownItContainer from 'markdown-it-container'
+import toProps from './util/toProps'
+
+export default function(md, options) {
+  md.use(MarkdownItContainer, 'details', {
+    render: renderProseDetails(md),
+    marker: '^'
+  })
+}
+
+function renderProseDetails (md) {
+  return (tokens, index) => {
+    const propsInterface = { summary: 'string' },
+          props = toProps(tokens[index].info, propsInterface)
+
+    return tokens[index].nesting === 1
+      ? `<ProseDetails v-bind="${props}">\n`
+      : '</ProseDetails>'
+  }
+}
--- src/stubs/proseRender.js
+++ src/stubs/proseRender.js
@@ -1,3 +1,4 @@
+// TODO: prism highlight
 import MarkdownIt from 'markdown-it'
 import { aside, blockquote, codeblock, grid, heading, details } from 'prose-plugins'
 
