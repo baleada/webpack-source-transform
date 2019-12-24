a938a439cfa9fdaf59e03b641dbd9c97980e6647
AlexVipond <alex.r.vipond@gmail.com>
2019-12-24 08:46:32 -0600

    remove old stuff

--- src/transforms/index.js
+++
@@ -1,9 +0,0 @@
-import toGrid from './toGrid.js'
-import toHighlighted from './toHighlighted.js'
-import toInnerHtml from './toInnerHtml.js'
-
-export {
-  toGrid,
-  toHighlighted,
-  toInnerHtml,
-}
--- src/transforms/toInnerHtml.js
+++
@@ -1,5 +0,0 @@
-export default function(markup) {
-  return markup
-    .replace(/^<(?:[A-Za-z0-9-]+?\s?)(.*?)>(?:\r?\n)*?/, '')
-    .replace(/(?:\r?\n)*?<\/[A-Za-z0-9-]*?>(?:\r?\n)*?$/, '')
-}
