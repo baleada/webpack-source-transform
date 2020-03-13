035e188be59bea0e963cb074c291472b1fcc0cbf
AlexVipond <alex.r.vipond@gmail.com>
2020-03-12 18:59:42 -0600

    upgrade ava

--- ava.config.js
+++ ava.config.js
@@ -1,14 +1,14 @@
 export default {
   files: [ 'tests/**/*.test.js' ],
-  helpers: [
-    'src/**/*.js',
-    'tests/test-util/*.js',
-  ],
   verbose: true,
   require: [
     './tests/test-util/setup.js'
   ],
   babel: {
+    compileAsTests: [
+      'src/**/*.js',
+      'tests/test-util/*.js',
+    ],
     testOptions: {
       presets: [
         [
